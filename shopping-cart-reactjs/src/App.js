e55966dbae55d22d69bb2556981b34ae981152a4
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-04-04 16:28:00 +0530

    updated

---
+++ shopping-cart-reactjs/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Navbar from './components/Navbar';
+import Shop from './components/Shop';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Shop />
+    </div>
+  )
+}
+
+export default App
